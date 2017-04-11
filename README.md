fdbb32c137b83364ce5e196319906dfd2ffef899
Mark Culp <markculp0@gmail.com>
2017-04-10 21:47:14 -0500

    1st commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# mtCars project
---
+++ mtcars.R
@@ -0,0 +1,8 @@
+# Regression Models Course : MTcars Analysis
+
+
+# [, 1]	 mpg	 Miles/(US) gallon
+# [, 2]	 cyl	 Number of cylinders
+# [, 3]	 disp	 Displacement (cu.in.)
+# [, 4]	 hp	 Gross horsepower
+# [, 9]	 am	 Transmission (0 = automatic, 1 = manual)
