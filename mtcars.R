e6c4ffd2b2b4f03ef060bb7853e68f06dfa5c196
Mark Culp <markculp0@gmail.com>
2017-04-30 14:22:54 -0500

    Executive summary and analysis detail; multivariable models

--- mtcars.R
+++ mtcars.R
@@ -1,21 +1,24 @@
+#===========================================
 # Regression Models Course : MTcars Analysis
+#===========================================
 
-
+# -----------
+# Key Columns
+# -----------
 # [, 1]	 mpg	 Miles/(US) gallon
 # [, 2]	 cyl	 Number of cylinders
+# [, 3]  disp  Displacement (Cubic inches)
 # [, 4]	 hp	 Gross horsepower
 # [, 6]	 wt  Weight (1000 lbs)
 # [, 9]	 am	 Transmission (0 = automatic, 1 = manual)
+# [,10]  gear  Number of forward gears
+# [,11]  carb  Number of carburetors
 
 # Load data set
 library(datasets)
 library(ggplot2)
 data("mtcars")
 
-# Subset manual transmissions
-mtcarMan <- mtcars[mtcars[,9] == 1,]
-# Automatic transmissions
-mtcarAuto <- mtcars[mtcars[,9] == 0,]
 
 ## --------------------
 ## Exploratory Analysis
@@ -32,36 +35,30 @@ range(mtcars$mpg)
 # 1 = manual 
 table(mtcars$am)
 
-## ---------------------------------
-## Fitting Multiple Models, Strategy
-## ---------------------------------
+## --------------------------------------
+## Fitting Simple Linear Regression (SLR)
+## --------------------------------------
 
 # So, we estimate a 7.25 mpg increase
 # in moving from a automatic to a manual
 # transmission.  We expect to get 17 mpg 
 # with an automatic transmission.
-fit1 <- lm(mpg ~ am, data = mtcars)
+fit1 <- lm(mpg ~ factor(am) -1, data = mtcars)
 coef(fit1)
 
-# This consistent with the mean values:
+# This is consistent with the mean values:
 # Automatic transmission:
-mean(mtcarAuto[,1])
+mean(mtcars[mtcars[,9] == 0,1])
 
 # Manual transmission:
-mean(mtcarMan[,1])
-
-# The fit is also consistent with mean for 
-# manual transmission:
-fit1$coefficients[1] + fit1$coefficients[2]
+mean(mtcars[mtcars[,9] == 1,1])
 
 # Summarize SLR comparison of auto vs man 
 # transmission
-summary(lm(mpg ~ factor(am) - 1 , mtcars))
+summary(fit1)$coef
 
-# And visually
-# g <- ggplot(mtcars,aes(x=am,y=mpg))
-# g <- g + geom_point()
-# g
+# So we can reject the null hypothesis that the transmission 
+# has no impact on MPG.
 
 boxplot(mpg ~ am, data = mtcars,
         boxwex = 0.4, at = 0:1 - 0.2,
@@ -72,20 +69,16 @@ boxplot(mpg ~ am, data = mtcars,
         xlim = c(-0.5,1), ylim = c(0,35), yaxs = "i")
 
 ## ---------------------------------
-library(car)
+## Fitting Multivariable Models
+## ---------------------------------
 
-# Examine fit with 4 variables 
-fit4 <- lm(mpg ~ factor(am) + cyl + hp + wt - 1, mtcars)
-summary(fit4)
+# And visually
+# g <- ggplot(mtcars,aes(x=am,y=mpg))
+# g <- g + geom_point()
+# g
 
-# See how correlated the variables are
-fit <- lm(mpg ~ ., mtcars)
-vif(fit)
-sqrt(vif(fit))
-
-# See how these regressors correlate with MPG
-cor(mtcars$disp,mtcars$wt)
-
+# Examine the correlation between MPG and some of the
+# regressors likely to impact it.
 regressor <- c("cyl","disp","hp","wt","am", "gear","carb")
 correlate <- c(round(cor(mtcars$mpg,mtcars$cyl),3),
                round(cor(mtcars$mpg,mtcars$disp),3),
@@ -96,13 +89,36 @@ correlate <- c(round(cor(mtcars$mpg,mtcars$cyl),3),
                round(cor(mtcars$mpg,mtcars$carb),3)
                )
 
+# Create data frame
 mtCor <- data.frame(regressor, correlate)
-t(mtCor[,2])
+# Display table of correlations
+t(mtCor)
+
+# Examine correlation between displacement
+# and weight.  
+cor(mtcars$disp,mtcars$wt)
+
+# Examine correlation between cylinders
+# and horse power.  
+cor(mtcars$cyl,mtcars$hp)
+
+# Update our model to include weight, cylinders,
+# and horse power.
+fit2 <- update(fit1, mpg ~ factor(am) + wt - 1)
+fit3 <- update(fit1, mpg ~ factor(am) + wt + cyl - 1)
+fit4 <- update(fit1, mpg ~ factor(am) + wt + cyl + hp - 1)
+
+# Examine differences between fit3 and fit4 to 
+# assess value of including horsepower as a variable.
+summary(fit3)
+summary(fit4)
+
+# coef(fit4)
 
 ## -----------------------------
 ## Residual Plot and Diagnostics
 ## -----------------------------
-
+library(car)
 
 g <- ggplot(data.frame(x = mtcars$am, y = resid(fit1)), aes(x=x, y=y))
 g <- g + ggtitle("Residual Plot (SLR)") 
@@ -112,10 +128,18 @@ g <- g + geom_point(size = 3, colour = "black")
 g <- g + geom_hline(yintercept = 0, size = 2)
 g <- g + scale_x_continuous(breaks = 0:1, limits = c(-0.5,1.5))
 g
-  
-## ------------------------------------
-## Conclusions : Quantified Uncertainty
-## ------------------------------------
+
+# Compute analysis of variance table  
+mtAnova <- anova(fit1,fit2,fit3,fit4)
+mtAnova
+
+
+# Examine variance inflation with 3 factors
+vif(lm(mpg ~ factor(am) + wt + cyl, mtcars))
+sqrt(vif(lm(mpg ~ factor(am) + wt + cyl, mtcars)))
+
+round(dfbetas(fit3),3)
+
 
 
 
